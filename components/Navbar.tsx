c4b9c339aa415949e8561d81cf385a0d82d9d600
Nadim Chowdhury <nadim-chowdhury@outlook.com>
2023-07-11 12:48:37 +0600

    first commit

---
+++ components/Navbar.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+export default function Navbar() {
+  return (
+    <nav className="max-w-3xl mx-auto px-4 sm:px-6 lg:px-8">
+      <div className="flex justify-between h-16">
+        <div className="flex justify-between items-center w-full">
+          <Link href=""></Link>
+        </div>
+      </div>
+    </nav>
+  );
+}
